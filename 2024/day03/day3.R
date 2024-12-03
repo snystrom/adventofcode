2bc8e2ebd1b3a8a15269a37c60b5332e0f98dd77
Spencer Nystrom <snystrom@users.noreply.github.com>
2024-12-03 00:43:11 -0500

    add part2.

--- 2024/day03/day3.R
+++ 2024/day03/day3.R
@@ -11,3 +11,18 @@ string %>%
   parse(text = .) %>%
   eval %>%
   print
+
+# Part 2
+string %>%
+  paste0(collapse = "") %>%
+  regmatches(., gregexpr("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don\'t\\(\\)", .)) %>%
+  .[[1]] %>%
+  gsub(",", "*", .) %>%
+  gsub("mul", "", .) %>%
+  paste0(collapse = "+") %>%
+  gsub("don\'t\\(\\).+?do\\(\\)\\+", "", .) %>%
+  gsub("don\'t\\(\\).+$|do\\(\\)\\+", "", .) %>%
+  gsub("\\+$", "", .) %>%
+  parse(text = .) %>%
+  eval %>%
+  print
