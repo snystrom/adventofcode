2e4d753ba7f98b97521de6b0cff544c800b9713a
Spencer <snystrom@users.noreply.github.com>
2024-12-07 16:27:08 -0500

    Day 7. For future reference, the site was bugged telling me my answers were wrong then later started working again. What a mess.

--- 2024/day07/day7.R
+++ 2024/day07/day7.R
@@ -1,13 +1,15 @@
 library(purrr)
 library(combinat)
+library(furrr)
+options(scipen = 99)
+plan("multicore", workers = 13)
 
-#eqns <- readLines("example.txt") %>%
 eqns <- readLines("input.txt") %>%
-  map(strsplit, ":") %>%
+  map(strsplit, ": ") %>%
   map(pluck, 1) %>%
   map(~{
     list(result = as.numeric(.x[1]),
-         values = as.numeric(strsplit(gsub("^ ", "", .x[2]), " ")[[1]])
+         values = as.numeric(strsplit(.x[2], " ")[[1]])
          )
   })
 
@@ -20,6 +22,7 @@ do_op <- function(x,y,op) {
   switch(op,
          "*" = x*y,
          "+" = x+y,
+         "||" = as.numeric(paste0(x,y)),
          stop("unknown operator")
          )
 }
@@ -33,58 +36,28 @@ do_ops <- function(values, ops){
   result
 }
 
-og_check_ops <- function(eqn) {
+check_ops <- function(eqn, ops = c("*", "+")) {
 
   values <- eqn$values
   result <- eqn$result
 
-  all_ops <- get_perm(c("*", "+"), length(values) - 1)
+  all_ops <- get_perm(ops, length(values) - 1)
 
   results <- apply(all_ops, 1, do_ops, values = values)
-  result %in% results
-}
-
-# For debugging...
-eval_ops <- function(eqn) {
-
-  values <- eqn$values
-  result <- eqn$result
-
-  all_ops <- get_perm(c("*", "+"), length(values) - 1)
-
-  results <- apply(all_ops, 1, do_ops, values = values)
-  list(results = results,
-       ops = all_ops)
-}
-
-check_ops <- function(eqn) {
-  eqn$result %in% eval_ops(eqn)$results
+  any(result == results)
 }
 
 
-# WRONG?!
-keep(eqns, og_check_ops) %>%
+# Part 1
+keep(eqns, check_ops) %>%
   map_dbl("result") %>%
-  sum
+  sum %>%
+  print
 
-
-out <- map(eqns, ~{
-  o <- eval_ops(.x)
-  .x$results <- o$results
-  .x$ops <- o$ops
-  .x
-})
-out[[1]]$result %in% out[[1]]$results
-
-
-map(out, ~{
-  .x$ops[.x$result == .x$results,,drop=FALSE]
-})
-
-keeps <- map_lgl(out, ~{
-  any(.x$result == .x$results)
-})
-
-map_dbl(out, "result") %>%
-  keep(keeps) %>%
-  sum
+# Part 2
+p2 <- future_map_lgl(eqns, check_ops, c("*", "+", "||"))
+eqns %>%
+  keep(p2) %>%
+  map_dbl("result") %>%
+  sum %>%
+  print
